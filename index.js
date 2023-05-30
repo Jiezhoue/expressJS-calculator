e83c1b1809730d2ed1b4f842b51d749cbe964b58
Jiezhoue <13809@coderacademy.edu.au>
2023-05-30 17:06:29 +1000

    use middleware to prefix the router

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const calculator = require("./router/calculator")
 
-
+app.use("/calculator", calculator)
 
 app.get("/", (req, res) => {
   res.send("Home Page");
